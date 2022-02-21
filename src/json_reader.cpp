f1cd2f093ab701d610b4171e3627673769536c2d
Matt Belanger <mattmbelanger@hotmail.com>
2022-02-21 14:14:33 -0500

    reader classes

---
+++ src/json_reader.cpp
@@ -0,0 +1,35 @@
+#include "json_reader.h"
+#include <iostream>
+#include <functional>
+#include <utility>
+
+
+void JsonReader::connect_handler(const boost::system::error_code& error){
+    if (!error){
+        std::cout << "accepted socket\n";
+
+        //read from server
+        
+        async_read_until(socket,boost::asio::dynamic_buffer(read,128), '\n',  [this](auto const& ec, auto size) { read_handler(ec,size); });
+
+    }else{
+        std::cout << "failed accept: ";
+        std::cout << error.what() << "\n";
+    }
+}
+
+void JsonReader::read_handler(const boost::system::error_code& error, std::size_t bytes_transferred){
+    if (!error){
+        std::cout << "accepted recieve "<< bytes_transferred <<"\n";
+    }else{
+        std::cout << "failed read\n";
+        std::cout << error.what();
+    }
+}
+
+void JsonReader::start(const std::string IPADDR, const unsigned short PORT){
+    //connect to server
+    socket.async_connect(ip::tcp::endpoint(ip::make_address(IPADDR.c_str()),PORT), [this](auto const& ec) { connect_handler(ec); });
+
+    io_context.run();
+}
---
+++ src/json_reader.h
@@ -0,0 +1,28 @@
+#if !defined(__DEMO__JSON_READER__H__)
+#define __DEMO__JSON_READER__H__
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/use_future.hpp>
+#include <boost/asio/write.hpp>
+
+
+using namespace boost::asio;
+
+class JsonReader{
+private:
+    io_context io_context;
+    boost::system::error_code error;
+    //create socket
+    ip::tcp::socket socket{io_context};
+
+public:
+    std::string read;
+    void connect_handler(const boost::system::error_code& error);
+    void read_handler(const boost::system::error_code& error, std::size_t bytes_transferred);
+
+    void start(const std::string IPADDR, const unsigned short PORT);
+};
+
+#endif
